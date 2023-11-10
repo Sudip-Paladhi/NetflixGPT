051a2132c09e13a24bff59e55f0a80fbb6c2f227
sudip paladhi <sudippaladhi@gmail.com>
2023-11-10 23:39:35 +0530

    setup React

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+const name1 ="sudip paladhi";
+
 const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(root)
+root.render({name1});
