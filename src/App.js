1d7450b28eb24682b474d684878ee6cd6a5e0fdf
sudip paladhi <sudippaladhi@gmail.com>
2023-11-25 17:12:32 +0530

    Authentication page updated

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Body from "./components/Body";
+
+const AppLayout = () => {
+    return (
+        <body />
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
