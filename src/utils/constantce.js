c225babce6fb18039fd581b75e6ff71c5fd30b2b
sudip paladhi <sudippaladhi@gmail.com>
2023-12-07 22:42:23 +0530

    Open AI page updated

--- src/utils/constantce.js
+++ src/utils/constantce.js
@@ -15,4 +15,4 @@ export const USER_AVATAR =
 
   export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-  export const OPEN_AI_KEY = "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";
+  export const OPEN_AI_KEY = "abcd";
