ebd142a90876cb42fc740ac871d1cc52f4c74628
sudip paladhi <sudippaladhi@gmail.com>
2023-12-09 23:58:06 +0530

    openAi data updated

--- src/utils/constantce.js
+++ src/utils/constantce.js
@@ -1,18 +1,21 @@
-export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-  export const API_OPTIONS = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTUyNjk0YWZiOWFjZjVkYTI4ZDc5NDExMjRkMjA3MiIsInN1YiI6IjY1Njg2ZDFlMTI3Nzc4MDlkYWIxZjc0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8tWTu8WwvgnU2pLeZqC36JAjUJ3BF7IELzu7RkuAVuc"
-    },
-  };
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOTUyNjk0YWZiOWFjZjVkYTI4ZDc5NDExMjRkMjA3MiIsInN1YiI6IjY1Njg2ZDFlMTI3Nzc4MDlkYWIxZjc0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8tWTu8WwvgnU2pLeZqC36JAjUJ3BF7IELzu7RkuAVuc",
+  },
+};
 
-  export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 
-  export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
+export const BG_URL =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-  export const OPEN_AI_KEY = "abcd";
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
