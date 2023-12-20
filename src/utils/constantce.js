ee50b36160f18d6bc6209789d2c0d4e3aba4385a
sudip paladhi <sudippaladhi@gmail.com>
2023-12-21 00:58:01 +0530

    API updated

--- src/utils/constantce.js
+++ src/utils/constantce.js
@@ -18,4 +18,4 @@ export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/";
 export const BG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/d1532433-07b1-4e39-a920-0f08b81a489e/67033404-2df8-42e0-a5a0-4c8288b4da2c/IN-en-20231120-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-export const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY;
+export const OPEN_AI_KEY = process.env.REACT_APP_OPENAI_API_KEY;
